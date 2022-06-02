d9b92891afa9b305f89b978ab238a5f15811de97
minnnk <jubal9153@gmail.com>
2022-06-02 18:01:07 +0900

    2022-06-02

---
+++ src/com/javaex/controller/UserController.java
@@ -0,0 +1,108 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.javaex.dao.UserDao;
+import com.javaex.util.WebUtil;
+import com.javaex.vo.UserVo;
+
+
+@WebServlet("/user")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+   
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		System.out.println("UserController");
+		
+		String action = request.getParameter("action");
+		
+		if("joinForm".equals(action)) {//회원가입폼
+			System.out.println("UserController>joinForm");
+			//회원가입 폼 포어드
+			WebUtil.forward(request, response, "/WEB-INF/views/user/joinForm.jsp");
+		}else if ("join".equals(action)) {//회원가입
+			System.out.println("UserController>join");
+			
+			//파라미터 꺼내기
+			String id = request.getParameter("id");
+			String password = request.getParameter("password");
+			String name = request.getParameter("name");
+			String gender = request.getParameter("gender");
+			
+			System.out.println(id);
+			//vo만들기
+			UserVo userVo = new UserVo(id,password,name,gender);
+			System.out.println(userVo);
+			//dao에 넣기
+			
+			UserDao userDao = new UserDao();
+			
+			userDao.insert(userVo);
+			
+			WebUtil.forward(request, response, "/WEB-INF/views/user/joinOk.jsp");
+		} else if ("loginForm".equals(action)) {
+			System.out.println("UserController>loginForm");
+			
+			WebUtil.forward(request, response, "/WEB-INF/views/user/loginForm.jsp");
+		} else if("login".equals(action)) {
+			System.out.println("UserController>login");
+			
+			//파라미터 꺼내기
+			String id = request.getParameter("id");;
+			String password = request.getParameter("password");
+			
+			//VO만들기
+			UserVo userVo = new UserVo();
+			userVo.setId(id);
+			userVo.setPassword(password);
+			
+			//DAO
+			UserDao userDao = new UserDao();
+			
+			UserVo authUser = userDao.getUser(userVo);
+			
+			// 
+			if(authUser == null) {
+				System.out.println("로그인실패");
+			}else {
+				System.out.println("로그인성공");
+				
+				HttpSession session =	request.getSession();
+				session.setAttribute("authUser",authUser);
+				
+				//메인 리다이렉트
+				
+				WebUtil.redirect(request, response, "/mysite2/main");
+			}	
+			
+		}else if("logout".equals(action)) {
+			System.out.println("UserController>logout");
+			
+			//세션값 지운다
+			HttpSession session= request.getSession();
+			session.removeAttribute("authUser");
+			session.invalidate();
+			
+			
+			WebUtil.redirect(request, response, "/mysite2/main");
+		}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/javaex/dao/UserDao.java
@@ -0,0 +1,131 @@
+package com.javaex.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.javaex.vo.UserVo;
+
+public class UserDao {
+	//필드
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String id = "webdb";
+	private String pw = "webdb";
+	//생성자
+	
+	//메소드 gs
+	
+	//메도스 일반
+	public void getConnecting() {
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver);
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} 	
+	}
+	
+	public void Close() {
+		try {
+			if (rs != null) {
+				rs.close();
+			} 
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+	}
+	
+	//회원가입 --> (db)insert
+	public int insert(UserVo userVo) {
+		int count = -1;
+		try {
+			this.getConnecting();
+			
+			
+			// 3. SQL문 준비 / 바인딩 / 실행
+			//SQl문 준비
+			String query = "";
+			query += " insert into users ";
+			query += " values(SEQ_USERS_NO.nextval,?,?,?,?) ";
+			
+			//바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, userVo.getId());
+			pstmt.setString(2, userVo.getPassword());
+			pstmt.setString(3, userVo.getName());
+			pstmt.setString(4, userVo.getGender());
+			
+			//실행
+			count = pstmt.executeUpdate();
+			
+			//결과처리
+			System.out.println(count+ "건 등록되었습니다");
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} 
+		
+		this.Close();
+		return count;
+		
+	}
+	
+	public UserVo getUser(UserVo userVo) {
+		UserVo authUser = null;
+		try {
+			this.getConnecting();
+			
+			
+			// 3. SQL문 준비 / 바인딩 / 실행
+			//SQl문 준비
+			String query = "";
+			query += " select  no ";
+			query += "         ,name  ";
+			query += " from users  ";
+			query += " where id = ?  ";
+			query += " and password = ?  ";
+			
+			//바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, userVo.getId());
+			pstmt.setString(2, userVo.getPassword());
+			//실행
+			rs = pstmt.executeQuery();
+			
+			//결과처리
+			while(rs.next()) {
+				int no = rs.getInt("no");
+				String name = rs.getString("name");
+				
+				authUser = new UserVo();
+				authUser.setNo(no);
+				authUser.setName(name);
+			}
+			
+			
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} 
+		
+		this.Close();
+		return authUser;
+	}
+	
+	
+}
